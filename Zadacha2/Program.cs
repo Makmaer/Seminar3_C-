d3192ce5f895becf659aeef19bd51337f14f39c0
Makmaer <instrojgrad@gmail.com>
2022-07-06 20:50:05 +0500

    задачи из семинара3

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿void Diapazon (int chetvert)
+{
+    if (chetvert == 1) Console.WriteLine("0 < x < + бесконечность; 0 < y < + бесконечность");
+    if (chetvert == 2) Console.WriteLine("0 > x > - бесконечность; 0 < y < + бесконечность");
+    if (chetvert == 3) Console.WriteLine("0 > x > - бесконечность; 0 > y > - бесконечность");
+    if (chetvert == 4) Console.WriteLine("0 < x < + бесконечность; 0 > y > - бесконечность");
+    else if (chetvert < 1 || chetvert > 4) Console.WriteLine("Такой четверти не существует"); 
+}
+
+Console.WriteLine("Введите номер четверти: ");
+int chetvert  = Convert.ToInt32(Console.ReadLine());
+Diapazon(chetvert);
