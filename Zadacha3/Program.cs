0496f2b47a8e16f98016a7061f4fe075b6f472d9
Makmaer <instrojgrad@gmail.com>
2022-07-06 21:01:19 +0500

    задачи из семинара3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿void KvadratChisel (int N)
+{for (int i = 1; i <= N; i++)
+    {
+    Console.Write(" " + i + " ");
+    Console.WriteLine(" " + i*i + " ");
+    }
+}
+
+Console.WriteLine("Введите ччисло ");
+int n  = Convert.ToInt32(Console.ReadLine());
+KvadratChisel(n);
